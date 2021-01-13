284750f28d356c398cbb4a64fe384a99517faceb
Ryan Flynn <ryanflynn44@gmail.com>
2021-01-13 12:17:29 -0500

    Figured out routes

--- basic.py
+++ basic.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello World!</h1>"
 
+@app.route('/home')
+def home():
+    return "<h1>Home Page</h1>"
+
 if __name__ == '__main__':
     app.run()
